c8691693eef7274759a0128e9d77a7697463dc6f
paulk_naanac <58757588+Admin368@users.noreply.github.com>
2026-02-26 08:18:46 +0800

    Project Setup

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "drizzle-kit";
+
+import { env } from "@/env";
+
+export default {
+	schema: "./src/server/db/schema.ts",
+	dialect: "postgresql",
+	dbCredentials: {
+		url: env.DATABASE_URL,
+	},
+	tablesFilter: ["curriculum-aware-rag-education-chatbot_*"],
+} satisfies Config;
